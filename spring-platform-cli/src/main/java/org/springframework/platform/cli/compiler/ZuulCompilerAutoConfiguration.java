ace5f21c58cf10dc81b12652b2294ef3c48b755e
Spencer Gibb <spencer@gibb.us>
2014-08-06 01:52:46 -0600

    support for @EnableZuulProxy

---
+++ spring-platform-cli/src/main/java/org/springframework/platform/cli/compiler/ZuulCompilerAutoConfiguration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.platform.cli.compiler;
+
+import org.codehaus.groovy.ast.ClassNode;
+import org.codehaus.groovy.control.CompilationFailedException;
+import org.codehaus.groovy.control.customizers.ImportCustomizer;
+import org.springframework.boot.cli.compiler.AstUtils;
+import org.springframework.boot.cli.compiler.CompilerAutoConfiguration;
+import org.springframework.boot.cli.compiler.DependencyCustomizer;
+
+/**
+ * @author Dave Syer
+ *
+ */
+public class ZuulCompilerAutoConfiguration extends CompilerAutoConfiguration {
+
+	@Override
+	public boolean matches(ClassNode classNode) {
+		return AstUtils.hasAtLeastOneAnnotation(classNode, "EnableZuulProxy");
+	}
+
+	@Override
+	public void applyDependencies(DependencyCustomizer dependencies) {
+		dependencies
+				.ifAnyMissingClasses("org.springframework.platform.netflix.zuul.EnableZuulProxy")
+				.add("spring-platform-starter-zuul");
+	}
+	
+	@Override
+	public void applyImports(ImportCustomizer imports) throws CompilationFailedException {
+		imports.addImports("org.springframework.platform.netflix.zuul.EnableZuulProxy");
+	}
+
+}
