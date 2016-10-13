f4211e9236b7ab7cfcf15a52a8fe1ed0da0aec51
Spencer Gibb <spencer@gibb.us>
2016-10-13 14:34:25 -0600

    Update SNAPSHOT to 1.2.0.RELEASE

--- spring-cloud-launcher/spring-cloud-launcher-deployer/src/main/java/org/springframework/cloud/launcher/deployer/DeployerThread.java
+++ spring-cloud-launcher/spring-cloud-launcher-deployer/src/main/java/org/springframework/cloud/launcher/deployer/DeployerThread.java
@@ -39,7 +39,7 @@ public class DeployerThread extends Thread {
 
 	private static final Logger logger = LoggerFactory.getLogger(DeployerThread.class);
 
-	private static final String DEFAULT_VERSION = "1.2.0.BUILD-SNAPSHOT";
+	private static final String DEFAULT_VERSION = "1.2.0.RELEASE";
 
 	private String[] args;
 
