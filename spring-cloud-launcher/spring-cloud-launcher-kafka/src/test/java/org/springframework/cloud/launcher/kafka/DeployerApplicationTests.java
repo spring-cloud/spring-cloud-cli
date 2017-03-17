269b955fb1c50e5497a1b4a1dfd2884b49c5fc61
Spencer Gibb <spencer@gibb.us>
2017-03-17 11:33:49 -0600

    fix test that fails with port already in use error

--- spring-cloud-launcher/spring-cloud-launcher-kafka/src/test/java/org/springframework/cloud/launcher/kafka/DeployerApplicationTests.java
+++ spring-cloud-launcher/spring-cloud-launcher-kafka/src/test/java/org/springframework/cloud/launcher/kafka/DeployerApplicationTests.java
@@ -1,13 +1,28 @@
 package org.springframework.cloud.launcher.kafka;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.SocketUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DeployerApplicationTests {
+
+	@BeforeClass
+	public static void before() {
+		System.setProperty("kafka.port", String.valueOf(SocketUtils.findAvailableTcpPort()));
+		System.setProperty("zk.port", String.valueOf(SocketUtils.findAvailableTcpPort()));
+	}
+
+	@AfterClass
+	public static void after() {
+		System.clearProperty("kafka.port");
+		System.clearProperty("zk.port");
+	}
 
 	@Test
 	public void contextLoads() {
