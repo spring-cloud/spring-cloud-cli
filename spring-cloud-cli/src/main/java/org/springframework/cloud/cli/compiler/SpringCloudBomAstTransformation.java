deb448b594f2d81b5ce30b4139630047d0f96425
Spencer Gibb <spencer@gibb.us>
2017-03-20 12:05:01 -0600

    Updates SNAPSHOTS to 1.3.0.RC1

--- spring-cloud-cli/src/main/java/org/springframework/cloud/cli/compiler/SpringCloudBomAstTransformation.java
+++ spring-cloud-cli/src/main/java/org/springframework/cloud/cli/compiler/SpringCloudBomAstTransformation.java
@@ -28,7 +28,7 @@ import org.springframework.boot.cli.compiler.GenericBomAstTransformation;
 @GroovyASTTransformation(phase = CompilePhase.CONVERSION)
 public class SpringCloudBomAstTransformation extends GenericBomAstTransformation {
 
-	private static final String SPRING_CLOUD_VERSION = "Camden.BUILD-SNAPSHOT";
+	private static final String SPRING_CLOUD_VERSION = "Dalston.RC1";
 
 	@Override
 	protected String getBomModule() {
