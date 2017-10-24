96a096de649ce78fb5fa4f65e95751a09a0c322b
Spencer Gibb <spencer@gibb.us>
2017-10-24 14:11:45 -0400

    Update to 1.4.0.RC1 for Finchley.RC1

--- spring-cloud-cli/src/main/java/org/springframework/cloud/cli/compiler/SpringCloudBomAstTransformation.java
+++ spring-cloud-cli/src/main/java/org/springframework/cloud/cli/compiler/SpringCloudBomAstTransformation.java
@@ -28,7 +28,7 @@ import org.springframework.boot.cli.compiler.GenericBomAstTransformation;
 @GroovyASTTransformation(phase = CompilePhase.CONVERSION)
 public class SpringCloudBomAstTransformation extends GenericBomAstTransformation {
 
-	private static final String SPRING_CLOUD_VERSION = "Edgware.BUILD-SNAPSHOT";
+	private static final String SPRING_CLOUD_VERSION = "Edgware.RC1";
 
 	@Override
 	protected String getBomModule() {
