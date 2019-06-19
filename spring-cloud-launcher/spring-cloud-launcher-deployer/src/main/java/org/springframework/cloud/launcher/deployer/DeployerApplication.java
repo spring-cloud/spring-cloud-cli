e7e734319ab544b23ff76d9e9438a4b309333f9d
Spencer Gibb <spencer@gibb.us>
2019-06-19 16:42:01 -0400

    Bumps cli to 2.2.0 and Hoxton

--- spring-cloud-launcher/spring-cloud-launcher-deployer/src/main/java/org/springframework/cloud/launcher/deployer/DeployerApplication.java
+++ spring-cloud-launcher/spring-cloud-launcher-deployer/src/main/java/org/springframework/cloud/launcher/deployer/DeployerApplication.java
@@ -42,7 +42,7 @@ public class DeployerApplication {
 	private static final Logger logger = LoggerFactory
 			.getLogger(DeployerApplication.class);
 
-	private static final String DEFAULT_VERSION = "2.1.0.BUILD-SNAPSHOT";
+	private static final String DEFAULT_VERSION = "2.2.0.BUILD-SNAPSHOT";
 
 	private String[] args;
 
