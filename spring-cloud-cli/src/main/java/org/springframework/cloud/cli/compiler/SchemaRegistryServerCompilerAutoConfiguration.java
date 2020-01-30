78368e2e43db32bda821dcc4a3697dc7ec55a15d
Eddú Meléndez <eddu.melendez@gmail.com>
2020-01-30 18:10:10 -0300

    Add SchemaRegistryServer
    See gh-143

---
+++ spring-cloud-cli/src/main/java/org/springframework/cloud/cli/compiler/SchemaRegistryServerCompilerAutoConfiguration.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.cli.compiler;
+
+import org.codehaus.groovy.ast.ClassNode;
+import org.codehaus.groovy.control.CompilationFailedException;
+import org.codehaus.groovy.control.customizers.ImportCustomizer;
+
+import org.springframework.boot.cli.compiler.AstUtils;
+import org.springframework.boot.cli.compiler.CompilerAutoConfiguration;
+import org.springframework.boot.cli.compiler.DependencyCustomizer;
+
+/**
+ * @author Eddú Meléndez
+ */
+public class SchemaRegistryServerCompilerAutoConfiguration extends CompilerAutoConfiguration {
+
+	@Override
+	public boolean matches(ClassNode classNode) {
+		return AstUtils.hasAtLeastOneAnnotation(classNode, "EnableSchemaRegistryServer");
+	}
+
+	@Override
+	public void applyDependencies(DependencyCustomizer dependencies) {
+		dependencies
+				.ifAnyMissingClasses("org.springframework.cloud.schema.registry.EnableSchemaRegistryServer")
+				.add("spring-cloud-schema-registry-core");
+	}
+
+	@Override
+	public void applyImports(ImportCustomizer imports) throws CompilationFailedException {
+		imports.addImports("org.springframework.cloud.schema.registry.EnableSchemaRegistryServer");
+	}
+}
