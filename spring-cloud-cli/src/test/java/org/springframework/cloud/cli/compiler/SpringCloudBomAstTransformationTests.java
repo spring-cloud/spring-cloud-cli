42a6a36f7f4713036edcf1d7b32c87566b1b36ce
spencergibb <spencer@gibb.us>
2021-02-08 13:56:07 -0500

    change test to digits for calver

--- spring-cloud-cli/src/test/java/org/springframework/cloud/cli/compiler/SpringCloudBomAstTransformationTests.java
+++ spring-cloud-cli/src/test/java/org/springframework/cloud/cli/compiler/SpringCloudBomAstTransformationTests.java
@@ -9,8 +9,7 @@ public class SpringCloudBomAstTransformationTests {
 	@Test
 	public void defaultVersionReadFromFile() {
 		String version = new SpringCloudBomAstTransformation().getBomVersion();
-		// starts with one or more word chars then a .
-		// TODO: change to \\d (digits) when merged to master
-		assertThat(version).isNotBlank().doesNotContainAnyWhitespaces().containsPattern("^\\w+\\..*");
+		// starts with one or more digits then a .
+		assertThat(version).isNotBlank().doesNotContainAnyWhitespaces().containsPattern("^\\d+\\..*");
 	}
 }
