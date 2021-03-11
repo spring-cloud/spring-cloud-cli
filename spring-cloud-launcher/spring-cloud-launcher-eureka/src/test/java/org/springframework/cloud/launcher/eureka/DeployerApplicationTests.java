55615bc8a6d4289fea4f39a694388ac276f3ab5b
spencergibb <spencer@gibb.us>
2021-03-11 15:03:34 -0500

    Disable config client for tests

--- spring-cloud-launcher/spring-cloud-launcher-eureka/src/test/java/org/springframework/cloud/launcher/eureka/DeployerApplicationTests.java
+++ spring-cloud-launcher/spring-cloud-launcher-eureka/src/test/java/org/springframework/cloud/launcher/eureka/DeployerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest("spring.cloud.bus.enabled=false")
+@SpringBootTest({"spring.cloud.bus.enabled=false", "spring.cloud.config.enabled=false"})
 public class DeployerApplicationTests {
 
 	@Test
